29960e1e53a20707c84bdd1716565c0dd98de399
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-14 00:06:02 -0500

    Interface Changes

---
+++ Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  VirtualTourist
+//
+//  Created by Deborah on 2/13/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import Foundation
---
+++ Convienent.swift
@@ -0,0 +1,9 @@
+//
+//  Convienent.swift
+//  VirtualTourist
+//
+//  Created by Deborah on 2/13/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import Foundation

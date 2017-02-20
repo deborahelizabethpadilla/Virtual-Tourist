26d6a55dcb8a8770866e6c30f5dab7f5d3445483
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-20 13:05:23 -0500

    CoreData

--- CoreData.swift
+++ CoreData.swift
@@ -6,4 +6,200 @@
 //  Copyright © 2017 Deborah. All rights reserved.
 //
 
-import Foundation
+import CoreData
+
+
+typealias BatchTask=(_ workerContext: NSManagedObjectContext) -> ()
+
+
+enum CoreDataNotifications : String{
+    
+    case TaskFinished = "TaskFinished"
+}
+
+    //CoreData Info
+
+struct CoreData {
+    
+    static let sharedInstance = CoreData(modelName: "Model")
+    
+    
+    fileprivate let model : NSManagedObjectModel
+    fileprivate let coordinator : NSPersistentStoreCoordinator
+    fileprivate let modelURL : URL
+    fileprivate let dbURL : URL
+    fileprivate let persistingContext : NSManagedObjectContext
+    fileprivate let backgroundContext : NSManagedObjectContext
+    let context : NSManagedObjectContext
+    
+    
+    init?(modelName: String){
+        
+        
+        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd") else {
+            print("Unable to find \(modelName)in the main bundle")
+            return nil}
+        
+        self.modelURL = modelURL
+        
+       
+        guard let model = NSManagedObjectModel(contentsOf: modelURL) else{
+            print("unable to create a model from \(modelURL)")
+            return nil
+        }
+        self.model = model
+        
+        
+        coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+        
+        persistingContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        persistingContext.name = "Persisting"
+        persistingContext.persistentStoreCoordinator = coordinator
+        
+        context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.parent = persistingContext
+        context.name = "Main"
+        
+       
+        backgroundContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        backgroundContext.parent = context
+        backgroundContext.name = "Background"
+        
+        let fm = FileManager.default
+        
+        guard let  docUrl = fm.urls(for: .documentDirectory, in: .userDomainMask).first else{
+            print("Unable to reach the documents folder")
+            return nil
+        }
+        
+        self.dbURL = docUrl.appendingPathComponent("model.sqlite")
+        
+        do{
+            try addStoreTo(coordinator: coordinator,
+                           storeType: NSSQLiteStoreType,
+                           configuration: nil,
+                           storeURL: dbURL,
+                           options: nil)
+        } catch {
+            print("unable to add store at \(dbURL)")
+        }
+    }
+    
+    func addStoreTo(coordinator coord : NSPersistentStoreCoordinator,
+                    storeType: String,
+                    configuration: String?,
+                    storeURL: URL,
+                    options : [AnyHashable: Any]?) throws{
+        
+        try coord.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: dbURL, options: nil)
+    }
+}
+
+
+    //Delete Data
+
+extension CoreData {
+    
+    func dropAllData() throws{
+
+        try coordinator.destroyPersistentStore(at: dbURL, ofType:NSSQLiteStoreType , options: nil)
+        
+        try addStoreTo(coordinator: self.coordinator, storeType: NSSQLiteStoreType, configuration: nil, storeURL: dbURL, options: nil)
+    }
+}
+
+extension CoreData {
+    
+    func performBackgroundBatchOperation(_ batch: @escaping BatchTask){
+        
+        backgroundContext.perform(){
+            batch(self.backgroundContext)
+            
+            do{
+                try self.backgroundContext.save()
+            }catch{
+                fatalError("Error while saving backgroundContext: \(error)")
+            }
+        }
+    }
+}
+
+extension CoreData {
+    
+    func performBackgroundImportingBatchOperation(_ batch: @escaping BatchTask) {
+        
+     
+        let tmpCoord = NSPersistentStoreCoordinator(managedObjectModel: self.model)
+        
+        
+        do{
+            try addStoreTo(coordinator: tmpCoord, storeType: NSSQLiteStoreType, configuration: nil, storeURL: dbURL, options: nil)
+        }catch{
+            fatalError("Error adding a SQLite Store: \(error)")
+        }
+        
+    
+        let moc = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        moc.name = "Importer"
+        moc.persistentStoreCoordinator = tmpCoord
+        
+     
+        moc.perform(){
+            batch(moc)
+            
+            do {
+                try moc.save()
+            }catch{
+                fatalError("Error saving importer moc: \(moc)")
+            }
+            
+            let nc = NotificationCenter.default
+            let n = Notification(name: Notification.Name(rawValue: CoreDataNotifications.TaskFinished.rawValue),
+                                 object: nil)
+            nc.post(n)
+        }
+    }
+}
+
+extension CoreData {
+    
+    func save() {
+       
+        context.performAndWait(){
+            
+            if self.context.hasChanges{
+                do{
+                    try self.context.save()
+                }catch{
+                    fatalError("Error while saving main context: \(error)")
+                }
+        
+                self.persistingContext.perform(){
+                    do{
+                        try self.persistingContext.save()
+                    }catch{
+                        fatalError("Error while saving persisting context: \(error)")
+                    }
+                }
+                
+                
+            }
+        }
+    }
+    
+    func autoSave(_ delayInSeconds : Int){
+        
+        if delayInSeconds > 0 {
+            print("Autosaving")
+            save()
+            
+            let delayInNanoSeconds = UInt64(delayInSeconds) * NSEC_PER_SEC
+            let time = DispatchTime.now() + Double(Int64(delayInNanoSeconds)) / Double(NSEC_PER_SEC)
+            
+            DispatchQueue.main.asyncAfter(deadline: time, execute: {
+                self.autoSave(delayInSeconds)
+            })
+            
+        }
+    }
+}
--- VirtualTourist/MapViewController.swift
+++ VirtualTourist/MapViewController.swift
@@ -14,9 +14,26 @@ class MapViewController: UIViewController {
 
     @IBOutlet var mapView: MKMapView!
     
+    var locations = [Pin]()
+    
+    var sharedContext: NSManagedObjectContext {
+        return (CoreDataStack.sharedInstance?.context)!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "OK", style: .plain, target: nil, action: nil)
+        title = "Virtual Tourist"
+        mapView.delegate = self as! MKMapViewDelegate
+        
+        self.locations = fetchAllPins()
+        loadAllPins()
+        
+        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: #selector(MapViewController.handleLongPress(_:)))
+        longPressRecogniser.minimumPressDuration = 1.0
+        mapView.addGestureRecognizer(longPressRecogniser)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,5 +42,50 @@ class MapViewController: UIViewController {
     }
 
 
+    func handleLongPress(_ getstureRecognizer : UIGestureRecognizer){
+        if getstureRecognizer.state != .began {
+            return
+        } else {
+            let touchPoint = getstureRecognizer.location(in: self.mapView)
+            let touchMapCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchMapCoordinate
+            let newPin = Pin(dictionary: ["longitude": annotation.coordinate.longitude as AnyObject, "latitude": annotation.coordinate.latitude as AnyObject], context: (sharedContext))
+            print("\(newPin.longitude) + \(newPin.latitude)")
+            mapView.addAnnotation(annotation)
+            CoreDataStack.sharedInstance?.save()
+        }
+    }
+    
+    func fetchAllPins() -> [Pin] {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Pin")
+        do {
+            return try sharedContext.fetch(fetchRequest) as! [Pin]
+        } catch let error as NSError {
+            print("Error fetching pins: \(error)")
+            return [Pin]()
+        }
+    }
+    
+    func loadAllPins() {
+        for pin in locations {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(pin.latitude!), longitude: CLLocationDegrees(pin.longitude!))
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        let coordinate = view.annotation?.coordinate
+        let photoVC = storyboard?.instantiateViewController(withIdentifier: "PhotoViewController") as! PhotoViewController
+        for pin in locations {
+            if (coordinate?.latitude == pin.latitude && coordinate?.longitude == pin.longitude) {
+                photoVC.pin = pin
+            }
+        }
+        navigationController?.pushViewController(photoVC, animated: true)
+    }
 }
+
+
 

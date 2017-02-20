101bbf9c4cb8bbca3d7a2079fa037973e443d03b
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-20 13:10:24 -0500

    Network

--- FlickrAssist.swift
+++ FlickrAssist.swift
@@ -7,3 +7,112 @@
 //
 
 import Foundation
+
+class FlickrClient: NSObject {
+    
+    var images = [String]()
+    static let sharedInstance = FlickrClient()
+    
+    func taskForGet(_ pinLatitude: NSNumber, pinLongitude: NSNumber, completionHandler: @escaping (_ success: Bool, _ pictures: [String]?, _ errorString: String?) -> Void) {
+        let methodParameters = [
+            Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.SearchMethod,
+            Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+            Constants.FlickrParameterKeys.BoundingBox: bboxString(pinLatitude, pinLongitude: pinLatitude),
+            Constants.FlickrParameterKeys.SafeSearch: Constants.FlickrParameterValues.UseSafeSearch,
+            Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+            Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+            Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback]
+        displayImagesFromFlickrBySearch(methodParameters as [String : AnyObject]) { (success, pictures, errorString) in
+            if success {
+                completionHandler(true, pictures, nil)
+            } else {
+                completionHandler(false, nil, errorString)
+            }
+        }
+    }
+    
+    func bboxString(_ pinLatitude: NSNumber, pinLongitude: NSNumber) -> String {
+        let minimumLon = max(Double(pinLongitude) - Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.0)
+        let minimumLat = max(Double(pinLatitude) - Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.0)
+        let maximumLon = min(Double(pinLongitude) + Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.1)
+        let maximumLat = min(Double(pinLatitude) + Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.1)
+        return "\(minimumLon),\(minimumLat),\(maximumLon),\(maximumLat)"
+    }
+    
+    func displayImagesFromFlickrBySearch(_ parameters: [String:AnyObject], completionHandler: @escaping (_ success: Bool, _ pictures: [String], _ errorString: String?) -> Void) -> URLSessionDataTask {
+        let session = URLSession.shared
+        let request = URLRequest(url: flickrURLFromParameters(parameters))
+        
+        let task = session.dataTask(with: request, completionHandler: { (data, response, error) in
+            
+            func displayError(_ error: String) {
+                print(error)
+            }
+            
+            guard (error == nil) else {
+                displayError("There was an error with your request: \(error)")
+                return
+            }
+            
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                displayError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            guard let data = data else {
+                displayError("No data was returned by the request!")
+                return
+            }
+            
+            var parsedResult: AnyObject!
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            } catch {
+                displayError("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            guard let stat = parsedResult[Constants.FlickrResponseKeys.Status] as? String, stat == Constants.FlickrResponseValues.OKStatus else {
+                displayError("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            guard let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:AnyObject] else {
+                displayError("Cannot find keys '\(Constants.FlickrResponseKeys.Photos)' in \(parsedResult)")
+                return
+            }
+            
+            guard let photosArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String: AnyObject]] else {
+                displayError("Cannot find key '\(Constants.FlickrResponseKeys.Photo)' in \(photosDictionary)")
+                return
+            }
+            
+            for x in 0...20 {
+                let photoDictionary = photosArray[x] as [String: AnyObject]
+                
+                guard let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String else {
+                    displayError("Cannot find key '\(Constants.FlickrResponseKeys.MediumURL)' in \(photoDictionary)")
+                    return
+                }
+                self.images.append(imageUrlString)
+            }
+            completionHandler(true, self.images, nil)
+        })
+        task.resume()
+        return task
+    }
+    
+    fileprivate func flickrURLFromParameters(_ parameters: [String:AnyObject]) -> URL {
+        var components = URLComponents()
+        components.scheme = Constants.Flickr.APIScheme
+        components.host = Constants.Flickr.APIHost
+        components.path = Constants.Flickr.APIPath
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        return components.url!
+    }
+}
--- Photos.swift
+++ Photos.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  Photos.swift
 //  VirtualTourist
 //
 //  Created by Deborah on 2/20/17.
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import CoreData
+
+class Photos: NSManagedObject {
+    
+    convenience init(dictionary: [String:AnyObject], context: NSManagedObjectContext) {
+        if let entity = NSEntityDescription.entity(forEntityName: "Photo", in: context) {
+            self.init(entity: entity, insertInto: context)
+            self.path = dictionary["path"] as? String
+            self.imageUrl = dictionary["imageUrl"] as? String
+        } else {
+            fatalError("Unable to find Entity name!")
+        }
+    }
+    
+}

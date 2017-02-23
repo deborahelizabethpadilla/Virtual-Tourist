9e90ed26f10326bcb8630dcdc0d0c92fb1b9b646
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-23 18:00:33 -0500

    Refactor Code

--- FlickrNetwork.swift
+++ FlickrNetwork.swift
@@ -46,43 +46,53 @@ class FlickrClient: NSObject {
         let task = session.dataTask(with: request, completionHandler: { (data, response, error) in
             
             func displayError(_ error: String) {
+                
                 print(error)
             }
             
             guard (error == nil) else {
+                
                 displayError("There was an error with your request: \(error)")
                 return
             }
             
             guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                
                 displayError("Your request returned a status code other than 2xx!")
                 return
             }
             
             guard let data = data else {
+                
                 displayError("No data was returned by the request!")
                 return
             }
             
             var parsedResult: AnyObject!
+            
             do {
                 parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                
             } catch {
+                
                 displayError("Could not parse the data as JSON: '\(data)'")
                 return
             }
             
             guard let stat = parsedResult[Constants.FlickrResponseKeys.Status] as? String, stat == Constants.FlickrResponseValues.OKStatus else {
+                
                 displayError("Flickr API returned an error. See error code and message in \(parsedResult)")
                 return
             }
             
             guard let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:AnyObject] else {
+                
                 displayError("Cannot find keys '\(Constants.FlickrResponseKeys.Photos)' in \(parsedResult)")
                 return
             }
             
             guard let photosArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String: AnyObject]] else {
+                
                 displayError("Cannot find key '\(Constants.FlickrResponseKeys.Photo)' in \(photosDictionary)")
                 return
             }
@@ -91,18 +101,23 @@ class FlickrClient: NSObject {
                 let photoDictionary = photosArray[x] as [String: AnyObject]
                 
                 guard let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String else {
+                    
                     displayError("Cannot find key '\(Constants.FlickrResponseKeys.MediumURL)' in \(photoDictionary)")
                     return
                 }
+                
                 self.images.append(imageUrlString)
             }
+            
             completionHandler(true, self.images, nil)
         })
+        
         task.resume()
         return task
     }
     
     fileprivate func flickrURLFromParameters(_ parameters: [String:AnyObject]) -> URL {
+        
         var components = URLComponents()
         components.scheme = Constants.Flickr.APIScheme
         components.host = Constants.Flickr.APIHost
@@ -110,9 +125,11 @@ class FlickrClient: NSObject {
         components.queryItems = [URLQueryItem]()
         
         for (key, value) in parameters {
+            
             let queryItem = URLQueryItem(name: key, value: "\(value)")
             components.queryItems!.append(queryItem)
         }
+        
         return components.url!
     }
 }
--- Photos.swift
+++ Photos.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+import CoreData
+
+
+class Photos: NSManagedObject {
+ 
+    
+    
+}

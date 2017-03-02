e45c42dee728f3c53da2b99610af082d6cc0e74a
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-02 17:05:22 -0500

    Model

--- VirtualTourist/AppDelegate.swift
+++ VirtualTourist/AppDelegate.swift
@@ -9,79 +9,16 @@
 import UIKit
 import CoreData
 
+import UIKit
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-    
-    fileprivate let sqlStore = "VirtualTourist.sqlite"
-    
+    let stack = CoreDataStack(modelName: "CoreDataModel")!
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
     
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
-    
-    //Core Data Stack
-    
-    
-    lazy var managedObjectContext: NSManagedObjectContext = {
-        let context = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType)
-        context.persistentStoreCoordinator = self.persistentStoreCoordinator
-        return context
-    }()
-    
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.appendingPathComponent(self.sqlStore)
-        let store = try? coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: nil)
-        if store == nil {
-            abort()
-        }
-        return coordinator
-    }()
-    
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        let url = Bundle.main.url(forResource: "CoreDataModel", withExtension: "momd")!
-        let model = NSManagedObjectModel(contentsOf: url)!
-        return model
-    }()
-    
-    lazy var applicationDocumentsDirectory: URL = {
-        let fileManager = FileManager.default
-        return fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-    }()
-    
-    func saveContext() {
-        if managedObjectContext.hasChanges {
-            try! managedObjectContext.save()
-        }
-    }
-}
-
-func sharedContext() -> NSManagedObjectContext {
-    return (UIApplication.shared.delegate as! AppDelegate).managedObjectContext
 }
